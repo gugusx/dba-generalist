5336618663ae06403bfe761211b20518a8f7c6b7
gugusx <gusmira94@hotmail.com>
2025-03-01 14:14:03 +0700

    ROMBS data collection and analysis query

--- queries-analytics/rombs_query.sql
+++ queries-analytics/rombs_query.sql
@@ -39,44 +39,44 @@ WITH
     LEFT JOIN "$VTABLE" c ON t.outlet_code = c.kode_outlet_lama
     GROUP BY 1
   ),
-  ls AS (
-    SELECT outlet_code, target_ls, linkselling_lm, linkselling_now,
-      round((linkselling_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric),2) as proyeksi_ls,
-      round(case when linkselling_now = 0 and target_ls = 0 then 0 when target_ls = 0 then 0 else linkselling_now/target_ls end*100,2) as acv_ls
+  p1 AS (
+    SELECT outlet_code, target_program1, program1_lastmonth, program1_now,
+      round((program1_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric),2) as proyeksi_program1,
+      round(case when program1_now = 0 and target_program1 = 0 then 0 when target_program1 = 0 then 0 else program1_now/target_program1 end*100,2) as acv_program1
     FROM "$VTABLE"
   ),
-  kp AS (
-    SELECT csv_outlet_id as outlet_code, target_trans_kp, trans_kp_lm, trans_kp_now,
-      round((trans_kp_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_kp,
-      round(case when trans_kp_now = 0 and target_trans_kp = 0 then 0 when target_trans_kp = 0 then 0 else trans_kp_now/target_trans_kp end*100,2) as acv_kp,
-      te.repurchase as target_repurchase, repurchase_lm, repurchase_now,
+  p2 AS (
+    SELECT csv_outlet_id as outlet_code, target_trans_program2, trans_program2_lastmonth, trans_program2_now,
+      round((trans_program2_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_program2,
+      round(case when trans_program2_now = 0 and target_trans_program2 = 0 then 0 when target_trans_program2 = 0 then 0 else trans_program2_now/target_trans_program2 end*100,2) as acv_program2,
+      te.repurchase as target_repurchase, repurchase_lastmonth, repurchase_now,
       round((repurchase_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_repurchase,
       round(case when repurchase_now = 0 and te.repurchase = 0 then 0 when te.repurchase = 0 then 0 else repurchase_now/te.repurchase end*100,2) as acv_repurchase
     FROM "$VTABLE" kp
     LEFT JOIN (select coalesce(kode_outlet_baru, kode) as kode, repurchase from "$VTABLE" te left join "$VTABLE" c on te.kode = c.kode_outlet_lama) as te
     ON kp.csv_outlet_id = te.kode
   ),
-  nm AS (
-    SELECT outlet_code, new_member as target_new_member, new_member_last_month, new_member_now, round((new_member_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_new_member, round(case when new_member_now = 0 and new_member = 0 then 0 when new_member = 0 then 0 else new_member_now/new_member end*100,2) as acv_new_member
+  p3 AS (
+    SELECT outlet_code, new_member as target_program3, program3_lastmonth, program3_now, round((program3_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_program3, round(case when program3_now = 0 and new_member = 0 then 0 when new_member = 0 then 0 else program3_now/new_member end*100,2) as acv_program3
     FROM "$VTABLE" nm
     LEFT JOIN (select coalesce(kode_outlet_baru, kode) as kode, new_member from "$VTABLE" te left join "$VTABLE" c on te.kode = c.kode_outlet_lama) as tnm
     ON nm.outlet_code = tnm.kode
   ),
-  dso AS (
-    SELECT location_id, target_dso, dso_lm, dso_now, round((dso_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_dso, round(case when dso_now = 0 and target_dso = 0 then 0 when target_dso = 0 then 0 else dso_now/target_dso end*100,2) as acv_dso, nominal_dso_lm, nominal_dso_now, round((nominal_dso_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_nominal_dso
+  p4 AS (
+    SELECT location_id, target_program4, program4_lastmonth, program4_now, round((program4_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_program4, round(case when program4_now = 0 and target_program4 = 0 then 0 when target_program4 = 0 then 0 else program4_now/target_program4 end*100,2) as acv_program4, nominal_program4_lastmonth, nominal_program4_now, round((nominal_program4_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_nominal_program4
     FROM "$VTABLE"
   ),
-  sa AS (
-    SELECT location_id, target_susu, susu_lm, susu_now, round((susu_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_susu, round(case when susu_now = 0 and target_susu = 0 then 0 when target_susu = 0 then 0 else susu_now/target_susu end*100,2) as acv_susu, target_alkes, alkes_lm, alkes_now, round((alkes_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_alkes, round(case when alkes_now = 0 and target_alkes = 0 then 0 when target_alkes = 0 then 0 else alkes_now/target_alkes end*100,2) as acv_alkes
+  p5 AS (
+    SELECT location_id, target_program5, program5_lastmonth, program5_now, round((program5_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_program5, round(case when program5_now = 0 and target_program5 = 0 then 0 when target_program5 = 0 then 0 else program5_now/target_program5 end*100,2) as acv_program5, target_program6, program6_lastmonth, program6_now, round((program6_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_program6, round(case when program6_now = 0 and target_program6 = 0 then 0 when target_program6 = 0 then 0 else program6_now/target_program6 end*100,2) as acv_program6
     FROM "$VTABLE"
   )
-SELECT o.outlet_code, o.city_name, outlet_name, region, status_manajemen, rom, am, omset_monthly_bulan_lalu, omset_daily_bulan_lalu, omset_monthly_bulan_ini, omset_daily_bulan_ini, prediksi_omset_monthly_bulan_ini, growth_omset_today, growth_omset_h_8, growth_omset_h_7, growth_omset_h_6, growth_omset_h_5, growth_omset_h_4, growth_omset_h_3, growth_omset_h_2, omset_today, margin, stock_value_today, stock_sku_today, target_bep, case when omset_monthly_bulan_lalu < target_bep then 'NO BEP' else 'BEP' end as bep_bulan_lalu, case when omset_monthly_bulan_ini < target_bep then 'NO BEP' else 'BEP' end as bep_bulan_ini, invoice_today, acb_today, target_ls, linkselling_lm, linkselling_now, proyeksi_ls, acv_ls, target_trans_kp, trans_kp_lm, trans_kp_now, proyeksi_kp, acv_kp, target_repurchase, repurchase_lm, repurchase_now, proyeksi_repurchase, acv_repurchase, target_new_member, new_member_last_month, new_member_now, proyeksi_new_member, acv_new_member, target_dso, dso_lm, dso_now, proyeksi_dso, acv_dso, nominal_dso_lm, nominal_dso_now, proyeksi_nominal_dso, target_susu, susu_lm, susu_now, proyeksi_susu, acv_susu, target_alkes, alkes_lm, alkes_now, proyeksi_alkes, acv_alkes, target_belanja_mingguan, pembelian_1, pembelian_2, pembelian_3, pembelian_4
+SELECT o.outlet_code, o.city_name, outlet_name, region, status_manajemen, rom, am, omset_monthly_bulan_lalu, omset_daily_bulan_lalu, omset_monthly_bulan_ini, omset_daily_bulan_ini, prediksi_omset_monthly_bulan_ini, growth_omset_today, growth_omset_h_8, growth_omset_h_7, growth_omset_h_6, growth_omset_h_5, growth_omset_h_4, growth_omset_h_3, growth_omset_h_2, omset_today, margin, stock_value_today, stock_sku_today, target_bep, case when omset_monthly_bulan_lalu < target_bep then 'NO BEP' else 'BEP' end as bep_bulan_lalu, case when omset_monthly_bulan_ini < target_bep then 'NO BEP' else 'BEP' end as bep_bulan_ini, invoice_today, acb_today, target_program1, program1_lastmonth, program1_now, proyeksi_program1, acv_program1, target_trans_program2, trans_program2_lastmonth, trans_program2_now, proyeksi_program2, acv_program2, target_repurchase, repurchase_lastmonth, repurchase_now, proyeksi_repurchase, acv_repurchase, target_program3, program3_lastmonth, program3_now, proyeksi_program3, acv_program3, target_program4, program4_lastmonth, program4_now, proyeksi_program4, acv_program4, nominal_program4_lastmonth, nominal_program4_now, proyeksi_nominal_program4, target_program5, program5_lastmonth, program5_now, proyeksi_program5, acv_program5, target_program6, program6_lastmonth, program6_now, proyeksi_program6, acv_program6, target_belanja_mingguan, pembelian_1, pembelian_2, pembelian_3, pembelian_4
 FROM otl o
 LEFT JOIN oms ON o.outlet_code = oms.outlet_code
 LEFT JOIN growth_7week g ON o.outlet_code = g.outlet_code
 LEFT JOIN target_bep tb ON o.outlet_code = tb.outlet_code
-LEFT JOIN ls ON o.outlet_code = ls.outlet_code
-LEFT JOIN kp ON o.outlet_code = kp.outlet_code
-LEFT JOIN nm ON o.outlet_code = nm.outlet_code
-LEFT JOIN dso ON o.outlet_code = dso.location_id
-LEFT JOIN sa ON o.outlet_code = sa.location_id;
+LEFT JOIN p1 ON o.outlet_code = p1.outlet_code
+LEFT JOIN p2 ON o.outlet_code = p2.outlet_code
+LEFT JOIN p3 ON o.outlet_code = p3.outlet_code
+LEFT JOIN p4 ON o.outlet_code = p4.location_id
+LEFT JOIN p5 ON o.outlet_code = p5.location_id;
