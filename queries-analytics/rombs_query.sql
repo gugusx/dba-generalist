3b63ee81a0436db6e6302422f859a2943127093d
gugusx <gusmira94@hotmail.com>
2025-03-01 13:05:34 +0700

    ROMBS data collection and analysis query

--- queries-analytics/rombs_query.sql
+++ queries-analytics/rombs_query.sql
@@ -8,13 +8,13 @@ VTABLE="customize with your table"
 
 --- QUERY FOR COLLECTING AND ANALYZING ROMBS DATA
 WITH
-  outlet AS (
+  otl AS (
     SELECT mo.outlet_code, mo.city_name, mo.outlet_name, mo.region, mo.status_manajemen, rom, am, mo.soft_ops_date
     FROM "$VTABLE" mo
     INNER JOIN "$VTABLE" m ON mo.outlet_code = m.outlet_code
     WHERE status_operation = 'ACTIVE' AND mo.outlet_code NOT LIKE '4%'
   ),
-  omset AS (
+  oms AS (
     SELECT coalesce(kode_outlet_baru, outlet_code) as outlet_code, omset_monthly_bulan_lalu, omset_daily_bulan_lalu, omset_monthly_bulan_ini, omset_daily_bulan_ini, omset_daily_bulan_ini*(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day)) as prediksi_omset_monthly_bulan_ini, omset_today, round(case when gross_profit_riil_today = 0 and revenue_non_internal_today = 0 then 0 when revenue_non_internal_today = 0 then 0 else gross_profit_riil_today/revenue_non_internal_today end*100,2) as margin, stock_value_today, stock_sku_today, invoice_today, acb_today, round((1.80*omset_monthly_bulan_ini)/cast(4 as numeric)) as target_belanja_mingguan, pembelian_1, pembelian_2, pembelian_3, pembelian_4
     FROM "$VTABLE" rr
     LEFT JOIN "$VTABLE" c ON rr.outlet_code = c.kode_outlet_lama
@@ -70,8 +70,8 @@ WITH
     SELECT location_id, target_susu, susu_lm, susu_now, round((susu_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_susu, round(case when susu_now = 0 and target_susu = 0 then 0 when target_susu = 0 then 0 else susu_now/target_susu end*100,2) as acv_susu, target_alkes, alkes_lm, alkes_now, round((alkes_now/cast((extract(day from current_date-1)) as numeric))*cast(extract(day from date_trunc('month', now()-interval '1' day)+interval '1' month-interval '1' day) as numeric)) as proyeksi_alkes, round(case when alkes_now = 0 and target_alkes = 0 then 0 when target_alkes = 0 then 0 else alkes_now/target_alkes end*100,2) as acv_alkes
     FROM "$VTABLE"
   )
-INSERT INTO summary_rombs SELECT o.outlet_code, o.city_name, outlet_name, region, status_manajemen, rom, am, omset_monthly_bulan_lalu, omset_daily_bulan_lalu, omset_monthly_bulan_ini, omset_daily_bulan_ini, prediksi_omset_monthly_bulan_ini, growth_omset_today, growth_omset_h_8, growth_omset_h_7, growth_omset_h_6, growth_omset_h_5, growth_omset_h_4, growth_omset_h_3, growth_omset_h_2, omset_today, margin, stock_value_today, stock_sku_today, target_bep, case when omset_monthly_bulan_lalu < target_bep then 'NO BEP' else 'BEP' end as bep_bulan_lalu, case when omset_monthly_bulan_ini < target_bep then 'NO BEP' else 'BEP' end as bep_bulan_ini, invoice_today, acb_today, target_ls, linkselling_lm, linkselling_now, proyeksi_ls, acv_ls, target_trans_kp, trans_kp_lm, trans_kp_now, proyeksi_kp, acv_kp, target_repurchase, repurchase_lm, repurchase_now, proyeksi_repurchase, acv_repurchase, target_new_member, new_member_last_month, new_member_now, proyeksi_new_member, acv_new_member, target_dso, dso_lm, dso_now, proyeksi_dso, acv_dso, nominal_dso_lm, nominal_dso_now, proyeksi_nominal_dso, target_susu, susu_lm, susu_now, proyeksi_susu, acv_susu, target_alkes, alkes_lm, alkes_now, proyeksi_alkes, acv_alkes, target_belanja_mingguan, pembelian_1, pembelian_2, pembelian_3, pembelian_4
-FROM outlet o
+SELECT o.outlet_code, o.city_name, outlet_name, region, status_manajemen, rom, am, omset_monthly_bulan_lalu, omset_daily_bulan_lalu, omset_monthly_bulan_ini, omset_daily_bulan_ini, prediksi_omset_monthly_bulan_ini, growth_omset_today, growth_omset_h_8, growth_omset_h_7, growth_omset_h_6, growth_omset_h_5, growth_omset_h_4, growth_omset_h_3, growth_omset_h_2, omset_today, margin, stock_value_today, stock_sku_today, target_bep, case when omset_monthly_bulan_lalu < target_bep then 'NO BEP' else 'BEP' end as bep_bulan_lalu, case when omset_monthly_bulan_ini < target_bep then 'NO BEP' else 'BEP' end as bep_bulan_ini, invoice_today, acb_today, target_ls, linkselling_lm, linkselling_now, proyeksi_ls, acv_ls, target_trans_kp, trans_kp_lm, trans_kp_now, proyeksi_kp, acv_kp, target_repurchase, repurchase_lm, repurchase_now, proyeksi_repurchase, acv_repurchase, target_new_member, new_member_last_month, new_member_now, proyeksi_new_member, acv_new_member, target_dso, dso_lm, dso_now, proyeksi_dso, acv_dso, nominal_dso_lm, nominal_dso_now, proyeksi_nominal_dso, target_susu, susu_lm, susu_now, proyeksi_susu, acv_susu, target_alkes, alkes_lm, alkes_now, proyeksi_alkes, acv_alkes, target_belanja_mingguan, pembelian_1, pembelian_2, pembelian_3, pembelian_4
+FROM otl o
 LEFT JOIN oms ON o.outlet_code = oms.outlet_code
 LEFT JOIN growth_7week g ON o.outlet_code = g.outlet_code
 LEFT JOIN target_bep tb ON o.outlet_code = tb.outlet_code
