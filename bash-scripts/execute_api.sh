9bdefe87650931b1e8fb283712d6ee5477a5d0a3
gugusx <gusmira94@hotmail.com>
2025-03-04 10:41:33 +0700

    Add script for execute_api.sh

---
+++ bash-scripts/execute_api.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#--- SETUP VARIABLE
+lastmonth=$(date -d "${date}-1 month" +%-m)
+lastyearmonth=$(date -d "${date}-1 month" +%Y)
+currentmonth=$(date +%-m)
+currentyear=$(date +%Y)
+appname="your_app"
+
+#--- GET IP HOST
+ip=$(hostname -I | awk '{print $1;}')
+ip_port=your_port
+
+
+if [ "$(date +%-d)" == "1" ]; then
+    for a in "api1" "api2" "api3" "api4"
+    do
+        echo "API $a"
+        stat_sync=$(curl -X POST "http://$ip:$ip_port/$appname/$a?month=$lastmonth&year=$lastyearmonth")
+        tanggal=$(date +"%Y-%m-%d %H:%M:%m")
+        status=$(echo "$stat_sync" | grep -oP '"message":\s*"\K[^"]+')
+        echo "$a, $status, $tanggal" > /path/to/lastmonth_log_API_$a.txt
+    done
+elif [ "$(date +%-d)" == "15" ]; then
+    for a in "api1" "api2" "api3" "api4"
+    do
+        echo "API $a"
+        stat_sync=$(curl -X POST "http://$ip:$ip_port/$appname/$a?month=$currentmonth&year=$currentyear")
+        tanggal=$(date +"%Y-%m-%d %H:%M:%m")
+        status=$(echo "$stat_sync" | grep -oP '"message":\s*"\K[^"]+')
+        echo "$a, $status, $tanggal" > /path/to/currentmonth_log_API_$a.txt
+    done
+else
+    echo "Skip"
+fi
